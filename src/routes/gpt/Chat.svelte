5298c31c5e2581dd190a136050c417763884f13b
Florian Rupprecht <33600480+nx10@users.noreply.github.com>
2024-03-18 14:11:14 -0400

    Whoops

--- src/routes/gpt/Chat.svelte
+++ src/routes/gpt/Chat.svelte
@@ -91,7 +91,7 @@
 					<p class="font-bold">{names[bubble.role]}</p>
 					<small class="opacity-50">{bubble.timestamp}</small>
 				</header>
-				<p class="whitespace-pre">{bubble.content}</p>
+				<p class="whitespace-pre-wrap">{bubble.content}</p>
 			</div>
 		{/each}
 		{#if loading}
