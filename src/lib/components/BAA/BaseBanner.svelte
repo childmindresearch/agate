0b3c89ee7196599dcd28e26f80d61e851f590beb
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-05-02 09:24:54 -0400

    Update to new skeleton themes

--- src/lib/components/BAA/BaseBanner.svelte
+++ src/lib/components/BAA/BaseBanner.svelte
@@ -1,16 +1,30 @@
 <script lang="ts">
+	import { modeCurrent } from '@skeletonlabs/skeleton';
 	export let title: string;
 	export let message: string;
 
 	export let variant: 'success' | 'error' | 'warning';
+	let currentClass: string;
 
-	const colorClass = `alert variant-soft-${variant}`;
+	const colorLightClass = `alert variant-soft-${variant}`;
+	const colorDarkClass = `alert variant-filled-${variant}`;
+
+	$: currentClass = $modeCurrent ? colorLightClass : colorDarkClass;
 </script>
 
 <!--Ensure dynamic classes aren't purged.-->
-<span class:hidden={'variant-soft-success variant-soft-error variant-soft-warning'} />
+<span
+	class:hidden={`
+		variant-soft-success 
+		variant-soft-error 
+		variant-soft-warning 
+		variant-filled-success 
+		variant-filled-error 
+		variant-filled-warning
+	`}
+/>
 
-<aside class={colorClass}>
+<aside class={currentClass}>
 	<div class="alert-message">
 		<h3 class="h3">{title}</h3>
 		<p>{message}</p>
