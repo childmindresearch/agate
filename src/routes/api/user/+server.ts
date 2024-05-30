0809163aa5f1fc1bd3f77dde6ab6275a80187b80
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-05-30 13:25:27 -0400

    fix: Update user header in GET request to use X-MS-CLIENT-PRINCIPAL-NAME

--- src/routes/api/user/+server.ts
+++ src/routes/api/user/+server.ts
@@ -1,6 +1,6 @@
 import { json } from '@sveltejs/kit';
 
 export async function GET({ request }) {
-	const user = request.headers.get('X-User');
+	const user = request.headers.get('X-MS-CLIENT-PRINCIPAL-NAME') || 'development.user@example.com';
 	return json({ user });
 }
