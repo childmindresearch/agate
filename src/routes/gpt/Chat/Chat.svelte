9e737a3bebf185e946cc380632fed0cbe28ca4e6
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-05-30 16:20:37 -0400

    Add a copy button (#49)

--- src/routes/gpt/Chat/Chat.svelte
+++ src/routes/gpt/Chat/Chat.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
 	import { getToastStore } from '@skeletonlabs/skeleton';
-	import SvelteMarkdown from 'svelte-markdown';
-	import Code from './Code.svelte';
-	import './chat.postcss';
+	import ChatBubble from './ChatBubble.svelte';
+	import UploadIcon from '$lib/icons/UploadIcon.svelte';
 
 	export let systemPrompt: string;
-	export let user = 'You';
 
 	let messages = [
 		{ role: 'system', content: systemPrompt, timestamp: new Date().toLocaleTimeString() }
@@ -17,17 +15,6 @@
 
 	const model = 'gpt-4o';
 	const toastStore = getToastStore();
-
-	const names: { [key: string]: string } = {
-		assistant: 'Agate',
-		user: capitalizeFirstLetter(user.split('.')[0]),
-		system: 'System'
-	};
-	const roleCss: { [key: string]: string } = {
-		assistant: 'bg-primary-500/30 mr-auto w-10/12',
-		user: 'bg-primary-200/30 ml-auto w-10/12',
-		system: 'bg-primary-800/30 w-full'
-	};
 
 	async function addUserMessage(event: KeyboardEvent) {
 		if (event.key !== 'Enter' || (event.key === 'Enter' && event.shiftKey)) return;
@@ -85,10 +72,6 @@
 		elemChat.scrollTo({ top: elemChat.scrollHeight, behavior: 'smooth' });
 	}
 
-	function capitalizeFirstLetter(string: string) {
-		return string.charAt(0).toUpperCase() + string.slice(1);
-	}
-
 	function uploadFile() {
 		const input = document.createElement('input');
 		input.type = 'file';
@@ -136,28 +119,18 @@
 		id="chat-container"
 		class="w-full max-h-[70vh] min-h-[100px] p-4 overflow-y-auto space-y-4"
 	>
-		{#each messages as bubble}
-			<div class={`${roleCss[bubble.role]} card p-4 rounded-tl-none space-y-2 `}>
-				<header class="flex justify-between items-center">
-					<p class="font-bold">{names[bubble.role]}</p>
-					<small class="opacity-90">{bubble.timestamp}</small>
-				</header>
-				<SvelteMarkdown source={bubble.content} renderers={{ code: Code }} />
-			</div>
+		{#each messages as message}
+			<ChatBubble {message} />
 		{/each}
-		{#if loading}
-			<p>Agate is typing...</p>
-		{/if}
-		{#if uploading}
-			<p>Agate is processing the document.</p>
-		{/if}
+		<p class:hidden={!loading}>Agate is typing...</p>
+		<p class:hidden={!uploading}>Agate is processing the document...</p>
 	</section>
 	<div
 		class="input-group input-group-divider grid-cols-[auto_1fr_auto] rounded-container-token border-surface-700 border-2"
 	>
-		<button class="input-group-shim border-r-2" on:click={uploadFile}
-			><i class="fas fa-file-upload text-lg text-black"></i></button
-		>
+		<button class="input-group-shim border-r-2" on:click={uploadFile}>
+			<UploadIcon class="text-lg text-black" />
+		</button>
 		<textarea
 			bind:value={currentMessage}
 			class="bg-transparent border-0 ring-0"
---
+++ src/routes/gpt/Chat/ChatBubble.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	import CopyIcon from '$lib/icons/CopyIcon.svelte';
+	import { getToastStore, popup, type PopupSettings } from '@skeletonlabs/skeleton';
+	import SvelteMarkdown from 'svelte-markdown';
+	import CodeBlock from './CodeBlock.svelte';
+	import './chat.postcss';
+
+	export let message: { role: string; content: string; timestamp: string };
+
+	const toastStore = getToastStore();
+
+	const names: { [key: string]: string } = {
+		assistant: 'Agate',
+		user: 'You',
+		system: 'System'
+	};
+	const roleCss: { [key: string]: string } = {
+		assistant: 'bg-primary-500/30 mr-auto w-10/12',
+		user: 'bg-primary-200/30 ml-auto w-10/12',
+		system: 'bg-primary-800/30 w-full'
+	};
+
+	function toClipboard(text: string) {
+		return () => {
+			navigator.clipboard.writeText(text);
+			toastStore.trigger({
+				message: 'Copied message to clipboard.',
+				background: 'variant-filled-success'
+			});
+		};
+	}
+
+	const popupHover: PopupSettings = {
+		event: 'hover',
+		target: 'popupHover',
+		placement: 'top'
+	};
+</script>
+
+<div
+	class="card p-[0.3rem] border-2 border-gray-300"
+	style="border-radius: 2rem"
+	data-popup="popupHover"
+>
+	<p>Copy</p>
+</div>
+
+<div class={`${roleCss[message.role]} card p-4 rounded-tl-none space-y-2 `}>
+	<header class="flex justify-between items-center">
+		<p class="font-bold">{names[message.role]}</p>
+		<div class="grid grid-flow-col-dense gap-2">
+			<button
+				on:click={toClipboard(message.content)}
+				class="[&>*]:pointer-events-none hover:opacity-70"
+				use:popup={popupHover}
+			>
+				<CopyIcon />
+			</button>
+			<small class="opacity-90">{message.timestamp}</small>
+		</div>
+	</header>
+	<SvelteMarkdown source={message.content} renderers={{ code: CodeBlock }} />
+</div>
