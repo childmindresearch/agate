0b73050109ad126626280ed99bec8385b0774285
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-08-09 10:09:56 -0400

    feat: improve list handling

---
+++ src/routes/gpt/Chat/Renderers/OrderedListItem.svelte
@@ -0,0 +1,15 @@
+<div>
+    <li>
+        <slot />
+    </li>
+</div>
+
+<style>
+    li {
+        list-style-type: decimal;
+    }
+
+    div {
+        margin-left: 1.5em;
+    }
+</style>
---
+++ src/routes/gpt/Chat/Renderers/UnorderedListItem.svelte
@@ -0,0 +1,15 @@
+<div>
+    <li>
+        <slot />
+    </li>
+</div>
+
+<style>
+    li {
+        list-style-type: disc;
+    }
+
+    div {
+        margin-left: 1.5em;
+    }
+</style>
