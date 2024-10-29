b50e39acafef769ad9ac8a54471460b821436083
Reinder Vos de Wael <reinder.vosdewael@childmind.org>
2024-10-29 13:31:05 -0400

    feat: Add Sonnet 3.5 V2

--- src/lib/components/chat/constants.ts
+++ src/lib/components/chat/constants.ts
@@ -1,8 +1,13 @@
-export type modelNames = 'GPT-4o' | 'Claude 3 Opus' | 'Claude 3.5 Sonnet V1';
+export type modelNames =
+	| 'GPT-4o'
+	| 'Claude 3 Opus'
+	| 'Claude 3.5 Sonnet V1'
+	| 'Claude 3.5 Sonnet V2';
 export type modelTags =
 	| 'gpt-4o'
 	| 'anthropic.claude-3-opus-20240229-v1:0'
-	| 'anthropic.claude-3-5-sonnet-20240620-v1:0';
+	| 'anthropic.claude-3-5-sonnet-20240620-v1:0'
+	| 'anthropic.claude-3-5-sonnet-20241022-v2:0';
 
 export const LlmModels: { name: modelNames; tag: modelTags }[] = [
 	{
@@ -16,5 +21,9 @@ export const LlmModels: { name: modelNames; tag: modelTags }[] = [
 	{
 		name: 'Claude 3.5 Sonnet V1',
 		tag: 'anthropic.claude-3-5-sonnet-20240620-v1:0'
+	},
+	{
+		name: 'Claude 3.5 Sonnet V2',
+		tag: 'anthropic.claude-3-5-sonnet-20241022-v2:0'
 	}
 ];
